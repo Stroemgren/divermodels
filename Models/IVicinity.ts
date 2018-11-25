44a2a182da9d537dd49342cde1764f22c51f19a8
Stroemgren <casper@stroemgren.dk>
2018-11-25 18:32:49 +0100

    Add radius to Vicinity

--- Models/IVicinity.ts
+++ Models/IVicinity.ts
@@ -4,9 +4,10 @@ export type ILocationType = 'site' | 'center';
 
 export interface IVicinity {
     coordinate: ICoordinate;
+    radius: number;
     locations: {
         locationType: ILocationType;
         locationId: string;
         distance: number;
-    }[]
+    }[];
 }
