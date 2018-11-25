1ee6f63f0d26eb35eb1f9c6b5ee8d3ddfac09bca
Stroemgren <casper@stroemgren.dk>
2018-11-25 17:10:10 +0100

    Add Location, Vicinity and index file

---
+++ Models/IVicinity.ts
@@ -0,0 +1,11 @@
+import { ICenter } from "./ICenter";
+import { ILocation } from "./ILocation";
+import { ISite } from "./ISite";
+
+export interface IVicinity {
+    location: ILocation;
+    items: {
+        distance: number;
+        item: ISite | ICenter
+    }
+}
